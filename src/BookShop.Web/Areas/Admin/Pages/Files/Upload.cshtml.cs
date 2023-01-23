e6daf80e4667b44904c6b188c6d9449a75b9138c
Hakim Ghods <ghods.hakim@hotmail.com>
2023-01-23 19:07:34 +0330

    upload

--- src/BookShop.Web/Areas/Admin/Pages/Files/Upload.cshtml.cs
+++ src/BookShop.Web/Areas/Admin/Pages/Files/Upload.cshtml.cs
@@ -1,12 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace BookShop.Web.Areas.Admin.Pages.Files
+namespace BookShop.Web.Areas.Admin.Pages.Files;
+
+public class UploadModel : PageModel
 {
-    public class UploadModel : PageModel
+    private readonly IWebHostEnvironment _env;
+
+    public UploadModel(IWebHostEnvironment env)
     {
-        public void OnGet()
+        _env = env;
+    }
+
+    [BindProperty]
+    public IFormFile ContentFile { get; set; }
+
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
         {
+            return Page();
         }
+
+        if (ContentFile != null)
+        {
+            using var ms1 = new MemoryStream();
+            ContentFile.CopyTo(ms1);
+            ms1.Position = 0;
+
+            var path = Path.Combine(_env.ContentRootPath, "Files", ContentFile.FileName);
+
+            using var stream = System.IO.File.Create(path);
+            stream.Position = 0;
+            await ContentFile.CopyToAsync(stream);
+
+
+        }
+
+
+
+        return RedirectToPage("./Index");
     }
 }
