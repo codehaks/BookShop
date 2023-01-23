c3ca70b7776ebd3d24772a333a29b124e46774b9
Hakim Ghods <ghods.hakim@hotmail.com>
2023-01-23 17:30:49 +0330

    User Count Component

---
+++ src/BookShop.Web/ViewComponents/UserCountViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShop.Web.ViewComponents;
+
+public class UserCountViewComponent:ViewComponent
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public UserCountViewComponent(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var count = await _userManager.Users.CountAsync();
+        return View("Default",count); 
+    }
+}
