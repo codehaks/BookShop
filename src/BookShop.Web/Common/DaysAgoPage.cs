3645a74165fa230a17832bb7269c6db4477abf95
Hakim Ghods <ghods.hakim@hotmail.com>
2023-02-07 18:10:40 +0330

    Days ago page

---
+++ src/BookShop.Web/Common/DaysAgoPage.cs
@@ -0,0 +1,9 @@
+﻿namespace BookShop.Web.Common;
+
+public abstract class DaysAgoPage: Microsoft.AspNetCore.Mvc.RazorPages.Page
+{
+    public int DaysAgo(DateTime timeCreated)
+    {
+        return (DateTime.Now - timeCreated).Days;
+    }
+}
