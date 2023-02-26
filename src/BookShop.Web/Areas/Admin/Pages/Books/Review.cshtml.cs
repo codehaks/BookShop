afb3f77b484923ebd6271a2f741ac11d682b5c6f
Hakim Ghods <ghods.hakim@hotmail.com>
2023-02-26 12:33:39 +0330

    review page and buttons

---
+++ src/BookShop.Web/Areas/Admin/Pages/Books/Review.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookShop.Web.Areas.Admin.Pages.Books
+{
+    public class ReviewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
