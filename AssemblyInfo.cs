1a1ebdb2944d6d20102769815cf426ad31f98548
Hakim Ghods <ghods.hakim@hotmail.com>
2023-05-08 13:51:39 +0330

    build

---
+++ AssemblyInfo.cs
@@ -0,0 +1,7 @@
+#pragma warning disable IDE0005 // Using directive is unnecessary.
+
+using System;
+
+#pragma warning restore IDE0005 // Using directive is unnecessary.
+
+[assembly: CLSCompliant(false)]
