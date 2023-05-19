32dc4a6dc362f57a6fef5b72ff78fdcacebdb406
Hakim Ghods <ghods.hakim@hotmail.com>
2023-05-19 11:47:50 +0330

    clean-up #5

--- src/BookShop/Infrastructure/DataModels/BookData.cs
+++ src/BookShop/Infrastructure/DataModels/BookData.cs
@@ -5,7 +5,7 @@ namespace BookShop.Infrastructure.DataModels;
 public class BookCategory
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public required string Name { get; set; }
 
     //public List<BookData> Books { get; set; }
 }
@@ -23,33 +23,30 @@ public class BookData
 {
     public int Id { get; set; }
 
-    public ICollection<RatingData> Ratings { get; set; }
+    public ICollection<RatingData>? Ratings { get; set; }
 
     [MaxLength(50)]
-    public string FileName { get; set; }
+    public required string FileName { get; set; }
 
     public int CategoryId { get; set; }
-    public BookCategory Category { get; set; }
+    public BookCategory? Category { get; set; }
 
     [MaxLength(50)]
-    public string Name { get; set; }
+    public required string Name { get; set; }
 
     public LanguageType Language { get; set; }
 
     [MaxLength(500)]
-    public string Description { get; set; }
+    public required string Description { get; set; }
 
     public int Price { get; set; }
 
     [MaxLength(250)]
-    public string Author { get; set; }
+    public required string Author { get; set; }
 
     public int Year { get; set; }
     public int Pages { get; set; }
 
     [MaxLength(1_000_000)]
     public byte[]? CoverImage { get; set; }
-
-    //public int BookCategoryId { get; set; }
-    //public BookCategory BookCategory { get; set; }
 }
