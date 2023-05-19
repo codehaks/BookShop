4f25d99356b1bcc3bf65b96d01658b2efe544803
Hakim Ghods <ghods.hakim@hotmail.com>
2023-05-19 11:48:53 +0330

    book item #5

--- src/BookShop/Application/Models/BookItem.cs
+++ src/BookShop/Application/Models/BookItem.cs
@@ -3,10 +3,10 @@ namespace BookShop.Application.Models;
 public class BookItem
 {
     public int Id { get; init; }
-    public string Name { get; init; }
+    public required string Name { get; init; }
     public int Price { get; init; }
-    public string CategoryName { get; set; }
+    public string? CategoryName { get; set; }
 
-    public string Language { get; set; }
+    public required string Language { get; set; }
     public byte[]? CoverImage { get; set; }
 }
