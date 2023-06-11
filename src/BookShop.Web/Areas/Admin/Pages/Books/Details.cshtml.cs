ccd1266e597f798e8d84c44296fe9a32d4f49970
Hakim Ghods <ghods.hakim@hotmail.com>
2023-06-11 13:02:48 +0330

    adding logs to book details

--- src/BookShop.Web/Areas/Admin/Pages/Books/Details.cshtml.cs
+++ src/BookShop.Web/Areas/Admin/Pages/Books/Details.cshtml.cs
@@ -7,16 +7,36 @@ namespace BookShop.Web.Areas.Admin.Pages.Books;
 public class DetailsModel : PageModel
 {
     private readonly IBookService _bookService;
+    private readonly ILogger<DetailsModel> _logger;
 
-    public DetailsModel(IBookService bookService)
+    public DetailsModel(IBookService bookService, ILogger<DetailsModel> logger)
     {
         _bookService = bookService;
+        _logger = logger;
     }
 
     public BookDetails Output { get; set; }
 
     public void OnGet(int bookId)
     {
+
+        if (bookId <= 0)
+        {
+            _logger.LogError("Invalid book ID: {BookId}", bookId);
+            return;
+        }
+
+        _logger.LogInformation("Fetching details for book with ID: {BookId}", bookId);
+
         Output = _bookService.GetDetails(bookId);
+
+        if (Output == null)
+        {
+            _logger.LogInformation("No details found for book with ID: {BookId}", bookId);
+            return;
+        }
+
+        _logger.LogInformation("Details retrieved successfully");
+
     }
 }
